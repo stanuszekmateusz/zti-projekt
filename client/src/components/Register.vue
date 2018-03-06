362943a786dcdc1c43be91416b36bc3258065fcc
stanuszekmateusz <stanuszekmateusz@gmail.com>
2018-03-06 19:40:15 +0100

    dodanie rejestracji użytkowników

---
+++ client/src/components/Register.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Rejestracja</h1>
+
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="email"/>
+      <br>
+
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password"/>
+      <br>
+      <button
+        @click="register">
+        zarejestruj sie</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: 'abc',
+      password: '123'
+    }
+  },
+  methods: {
+    async  register () {
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
